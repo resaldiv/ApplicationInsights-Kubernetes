bd6b3bb0a952fe61f8b57ddd9e794b1255181a24
resaldiv <resaldiv@microsoft.com>
2024-11-04 11:39:02 -0800

    Fix

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 var base64 = require('js-base64').Base64;
-const { Octokit, App } = require("octokit");
 const fs = require('fs');
 const path = require('path');
+(async () => {
+  const { Octokit, App } = await import("octokit");
+})();
 
 DEEPPROMPT_ENDPOINT = "https://data-ai.microsoft.com/deepprompt/api/v1"
 
