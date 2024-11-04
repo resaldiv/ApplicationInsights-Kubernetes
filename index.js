3969f4ca62752ae0b2b8e84a255e4cd02e8bdcb3
resaldiv <resaldiv@microsoft.com>
2024-11-04 12:59:39 -0800

    Add new changes

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 var base64 = require('js-base64').Base64;
+const {Octokit} = require("@octokit/rest");
 const fs = require('fs');
 const path = require('path');
-const {Octokit} = require("@octokit/rest");
 
-DEEPPROMPT_ENDPOINT = "https://data-ai.microsoft.com/deepprompt/api/v1"
+DEEPPROMPT_ENDPOINT = "https://data-ai.microsoft.com/deepprompt/api/v1";
 
-async function run() {        
+async function run() {
     try {
-        console.log("---DeepPrompt Auth---");
-        const pat_token = core.getInput('pat-token');
+        // Repo metadata
+        const repo = core.getInput('repo');
+        const repo_token = core.getInput('repo-token');
+        const repo_url = `https://github.com/${repo}`;
+
+        // DeepPrompt Auth
+        const pat_token = core.getInput('token');
         const auth = await get_deepprompt_auth(pat_token);
         const auth_token = auth['access_token'];
         const session_id = auth['session_id'];
 
-        const repo_token = core.getInput('repo-token');
-        const repo = core.getInput('repo');
-        const repo_url = `https://github.com/${repo}`;
         const comment = core.getInput('comment', { required: false });
-        
         if (comment) {
             const pr_body = core.getInput('pr-body');
             const comment_id = core.getInput('comment-id');
@@ -32,51 +33,48 @@ async function run() {
             const response = await get_response(auth_token, session_id, query);
             post_comment(repo_token, repo_url, pr_number, response);
         } else {
-            console.log("---Issue info---");
-            const issue_body = core.getInput('issue-body');
+            // Issue metadata
             const issue_title = core.getInput('issue-title');
+            const issue_body = core.getInput('issue-body');
             const issue_number = core.getInput('issue-number');
 
-            console.log("---Symbol info---");
+            // Symbols
             const parent_symbol = issue_body.split('<!-- ps: ')[1].split(' -->')[0];
             const child_symbol = issue_body.split('<!-- s: ')[1].split(' -->')[0];
             const parent_class_name = parent_symbol.split('!')[0].split('.').at(-1);
             const parent_method_name = parent_symbol.split('!')[1];
             const child_method_name = child_symbol.split('!')[1];
 
-            console.log("---Files---");
+            // Files
             const path_ending = `${parent_class_name}.cs`;
             const found_files = searchFiles('./', path_ending);
             console.log(`Found files for ${path_ending}: ${found_files.join('\n')}`);
 
-            console.log("---Localization---");
-            const localization = await getLocalizationValues(found_files, parent_class_name, parent_method_name, child_method_name);
+            // Localization
+            const localization = await get_localization_values(found_files, parent_class_name, parent_method_name, child_method_name);
             const buggy_file_path = localization[0];
             const buggy_method_name = localization[1];
             const buggy_range = localization[2];
             const buggy_file_data = localization[3];
-            console.log(`Buggy file path: ${buggy_file_path}`);
-            console.log(`Buggy method name: ${buggy_method_name}`);
-            console.log(`Buggy range: ${buggy_range}`);
-            console.log(`Buggy file data:\n${buggy_file_data}`);
 
-            console.log("---DeepPrompt response---");
+            // DeepPrompt response
             const start_line_number = parseInt(buggy_range[0]);
             const end_line_number = parseInt(buggy_range[1]);
             const deepprompt_response = await get_deepprompt_response(auth_token, session_id, buggy_file_data, start_line_number, buggy_method_name);
 
-            console.log("---Clean up response---");
-            const code_text_array = deepprompt_response.match(/```([^`]*)```/);
-            const code_text = code_text_array[1];
+            // Clean up response
+            const code_text = deepprompt_response.match(/```([^`]*)```/)[1];
             const code_to_remove = `csharp\n\n`;
             const clean_code_text = code_text.substring(code_text.indexOf(code_to_remove) + code_to_remove.length);
 
-            console.log("---Fixed file---");
+            // Fixed file
             const fixed_file = fix_file(buggy_file_data, start_line_number, end_line_number, clean_code_text)
             console.log(fixed_file);
 
-            console.log("---Create PR---");
-            await create_pr(repo_token, repo_url, buggy_file_path, issue_title, issue_number, fixed_file, session_id);
+            // Create branch
+            const octokit = new Octokit({ auth: repo_token });
+            const branch = await create_branch(octokit, repo_url);
+            console.log(branch);
         }
     } catch (error) {
         core.setFailed(error.message);
@@ -121,34 +119,13 @@ async function post_comment(access_token, repo_url, pr_number, comment)
     console.log(data);
 }
 
-async function get_response(auth_token, session_id, query)
-{
-    let response = await fetch(`${DEEPPROMPT_ENDPOINT}/query`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'DeepPrompt-Version': 'v1',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${auth_token}`,
-            'DeepPrompt-Session-ID': session_id
-        },
-        body: JSON.stringify({
-            'query': query,
-        })
-    });
-    let data = await response.json();
-    let response_text = data['response_text'];
-    console.log("---------------");
-    console.log(data);
-    return response_text;
-}
-
 async function get_deepprompt_response(auth_token, session_id, buggy_file_data, start_line_number, buggy_method_name)
-{
+{   
+    const url = `${DEEPPROMPT_ENDPOINT}/query`;
     const intent = 'perf_fix';
     const prompt_strategy = 'instructive';
     try {
-        const response = await fetch(`${DEEPPROMPT_ENDPOINT}/query`, {
+        const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -170,117 +147,11 @@ async function get_deepprompt_response(auth_token, session_id, buggy_file_data, 
         });
         const response_json = await response.json();
         if (response_json["error"]) {
-            core.setFailed(response_json["error"].message);
-            return "";
+            core.setFailed(`The DeepPrompt service returned an error: ${response_json["error"].message}`);
         }
         return response_json["response_text"];
     } catch (error) {
-        console.log("Error in obtaining a response from DeepPrompt");
-        core.setFailed(error.message);
-    }
-}
-
-async function get_deepprompt_auth(pat_token) {
-    try {
-        const response = await fetch(`${DEEPPROMPT_ENDPOINT}/exchange`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                'token': pat_token,
-                'provider': 'github'
-            })
-        });
-        const auth_token = await response.json();
-        if (auth_token["error"]) {
-            console.log("DeepPrompt Auth response error");
-            core.setFailed(auth_token["error"].message);
-        }
-        return auth_token;
-    }
-    catch (error) {
-        console.log("DeepPrompt Auth catch error");
-        core.setFailed(error.message);
-    }
-}
-
-async function get_file(access_token, repo_url, buggy_file_path) {
-    const user = repo_url.split('/')[3];
-    const repo = repo_url.split('/')[4];
-    try {
-        url = `https://api.github.com/repos/${user}/${repo}/contents/${buggy_file_path}`
-        let response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `token ${access_token}`
-            }
-        });
-        let data = await response.json();
-        let file = base64.decode(data.content);
-        return file;
-    }
-    catch (error) {
-        core.setFailed(error.message);
-    }
-}
-
-function find_end_of_function(data, start_line_number) {
-    var lines = data.split('\n');
-    var i = start_line_number;
-    var open_braces = 0;
-    while (i < lines.length) {
-        var line = lines[i];
-        for (var j = 0; j < line.length; j++) {
-            if (line[j] == '{') {
-                open_braces++;
-            }
-            else if (line[j] == '}') {
-                open_braces--;
-            }
-        }
-        if (open_braces == 0) {
-            return i;
-        }
-        i++;
-    }
-    return i;
-}
-
-async function create_pr(repo_token, repo_url, buggy_file_path, issue_title, issue_number, fixed_file, session_id) {
-    const user = repo_url.split('/')[3];
-    const repo = repo_url.split('/')[4];
-
-    const fix_title = `PERF: Fix ${issue_title}`;
-    const branch_name = 'test-branch-' + (new Date()).getTime();
-
-    const octokit = new Octokit({ auth: repo_token });
-
-    let develop_sha;
-    try {
-        const response = await octokit.git.getRef({
-            owner: user,
-            repo: repo,
-            ref: 'heads/develop'
-        });
-        develop_sha = response.data.object.sha;
-        console.log(`The develop SHA is: ${develop_sha}`);
-    } catch (error) {
-        console.log(error);
-    }
-
-    try {
-        const response = await octokit.rest.git.createRef({
-            owner: user,
-            repo: repo,
-            ref: `refs/heads/${branch_name}`,
-            sha: develop_sha
-        });
-        console.log(response);
-    } catch (error) {
-        console.log(error);
+        core.setFailed(`An error occurred while trying to make a DeepPrompt request: ${error.message}`);
     }
 }
 
@@ -306,16 +177,148 @@ function fix_file(buggy_file_data, start_line_number, end_line_number, clean_cod
         const fixed_lines = lines.slice(0, start_line_number - 1).concat(formatted_clean_code_text.split('\n')).concat(lines.slice(end_line_number));
         return fixed_lines.join('\n');
     } catch(error) {
-        console.log(error);
-        core.setFailed(`An error occured while fixing the file: ${error}`);
+        core.setFailed(`An error occured while trying to fix the file: ${error.message}`);
     }
 }
 
-async function getLocalizationValues(found_files, parent_class_name, parent_method_name, child_method_name) {
+async function get_deepprompt_auth(pat_token) {
+    try {
+        const url = `${DEEPPROMPT_ENDPOINT}/exchange`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                'token': pat_token,
+                'provider': 'github'
+            })
+        });
+        const auth_token = await response.json();
+        if (auth_token["error"]) {
+            core.setFailed(`The DeepPrompt service returned an error: ${auth_token["error"].message}`);
+        }
+        return auth_token;
+    }
+    catch (error) {
+        core.setFailed(`An error occurred while trying to make a DeepPrompt request: ${error.message}`);
+    }
+}
+
+async function get_file(access_token, repo_url, buggy_file_path) {
+    const user = repo_url.split('/')[3];
+    const repo = repo_url.split('/')[4];
+    try {
+        url = `https://api.github.com/repos/${user}/${repo}/contents/${buggy_file_path}`
+        let response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `token ${access_token}`
+            }
+        });
+        let data = await response.json();
+        let file = base64.decode(data.content);
+        return file;
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function find_end_of_function(code, start_line_number) {
+    var lines = code.split('\n');
+    var i = start_line_number;
+    var open_braces = 0;
+    while (i < lines.length) {
+        var line = lines[i];
+        for (var j = 0; j < line.length; j++) {
+            if (line[j] == '{') {
+                open_braces++;
+            }
+            else if (line[j] == '}') {
+                open_braces--;
+            }
+        }
+        if (open_braces == 0) {
+            return i;
+        }
+        i++;
+    }
+    return i;
+}
+
+async function create_branch(octokit, repo_url) {
+    const user = repo_url.split('/')[3];
+    const repo = repo_url.split('/')[4];
+
+    let develop_sha;
+    try {
+        const response = await octokit.git.getRef({
+            owner: user,
+            repo: repo,
+            ref: 'heads/develop'
+        });
+        if (response.error) {
+            core.setFailed(`The GitHub API returned an error: ${response.error.message}`);
+        }
+        develop_sha = response.data.object.sha;
+    } catch (error) {
+        core.setFailed(`An error occurred while trying to get the ref SHA: ${error.message}`);
+    }
+
+    try {
+        const response = await octokit.rest.git.createRef({
+            owner: user,
+            repo: repo,
+            ref: `refs/heads/${branch_name}`,
+            sha: develop_sha
+        });
+        if (response.error) {
+            core.setFailed(`The GitHub API returned an error: ${response.error.message}`);
+        }
+        return response.data;
+    } catch (error) {
+        core.setFailed(`An error occurred while trying to create a new branch: ${error.message}`);
+    }
+}
+
+async function create_pr(access_token, repo_url, buggy_file_path, issue_title, issue_number, file, fixed_file, session_id) {
+    const user = repo_url.split('/')[3];
+    const repo = repo_url.split('/')[4];
+    const fix_title = `PERF: Fix ${issue_title}`;
+    const branch_name = 'test-branch-' + (new Date()).getTime();
+
+    const octokit = new MyOctokit({
+        auth: access_token,
+    });
+
+    var change = {}
+    change[buggy_file_path] = fixed_file;
+    octokit.createPullRequest({
+        owner: user,
+        repo: repo,
+        title: fix_title,
+        body: `Auto-generated PR fixing issue #${issue_number}. Session ID: ${session_id}.`,
+        head: branch_name,
+        base: 'main',
+        update: false,
+        forceFork: false,
+        changes: [
+            {
+                files: change,
+                commit: fix_title,
+            },
+        ],
+    });
+}
+
+async function get_localization_values(found_files, parent_class_name, parent_method_name, child_method_name) {
     for (let i = 0; i < found_files.length; i++) {
         const file = found_files[i];
         const file_data = fs.readFileSync(file, 'utf8');
-        const buggy_range = getBuggyRange(
+        const buggy_range = get_buggy_range(
             file_data,
             parent_class_name,
             parent_method_name,
@@ -329,7 +332,7 @@ async function getLocalizationValues(found_files, parent_class_name, parent_meth
     for (let i = 0; i < found_files.length; i++) {
         const file = found_files[i];
         const file_data = fs.readFileSync(file, 'utf8');
-        const buggy_range = getBuggyRange(
+        const buggy_range = get_buggy_range(
             file_data,
             parent_class_name,
             parent_method_name,
@@ -344,18 +347,18 @@ async function getLocalizationValues(found_files, parent_class_name, parent_meth
     return ["", "", [], ""];
 }
 
-function getBuggyRange(file_data, parent_class_name, parent_method_name, child_method_name, ignore_bottleneck = false) {
+function get_buggy_range(file_data, parent_class_name, parent_method_name, child_method_name, ignore_bottleneck = false) {
     const parent_function_signature = parent_method_name !== "ctor" ? `${parent_method_name}(` : `${parent_class_name}(`;
     const child_function_signature = `${child_method_name}(`;
     
-    const possible_starts = findAllOccurrences(file_data, parent_function_signature);
+    const possible_starts = find_all_occurrences(file_data, parent_function_signature);
     for (let i = 0; i < possible_starts.length; i++) {
         const start = possible_starts[i];
-        const end = getBalancedEndIndex(file_data.substring(start));
+        const end = get_balanced_end_index(file_data.substring(start));
 
         const file_data_block = file_data.substring(start, start + end);
         if (file_data_block.includes(child_function_signature)) {
-            const bug_starts = findAllOccurrences(file_data_block, child_function_signature);
+            const bug_starts = find_all_occurrences(file_data_block, child_function_signature);
             if (bug_starts.length > 0) {
                 const start_line_number = file_data.substring(0, start).split("\n").length;
                 const end_line_number = file_data.substring(0, start + end).split("\n").length;
@@ -373,7 +376,7 @@ function getBuggyRange(file_data, parent_class_name, parent_method_name, child_m
     return [];
 }
 
-function findAllOccurrences(data, function_signature) {
+function find_all_occurrences(data, function_signature) {
     let result = [];
     let position = data.indexOf(function_signature);
     while (position !== -1) {
@@ -383,7 +386,7 @@ function findAllOccurrences(data, function_signature) {
     return result;
 }
 
-function getBalancedEndIndex(data) {
+function get_balanced_end_index(data) {
   let open_count = 0;
   let index = 0;
   while (index < data.length) {
